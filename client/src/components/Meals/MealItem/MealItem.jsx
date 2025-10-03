4c6028faa7bef7b9d294b4cf96eed60820146669
chandrika-webkorps <chandrika.agarwal@webkorps.com>
2025-10-03 10:27:04 +0530

    form for adding the items done

--- client/src/components/Meals/MealItem/MealItem.jsx
+++ client/src/components/Meals/MealItem/MealItem.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import MealItemForm from './MealItemForm'
 function MealItem(props) {
   const price=`$${props.meal.price.toFixed(2)}`
   return <li>
+    <div className='flex justify-between items-center m-4'>
     <div>
         <h3 className='text-lg font-bold ml-6 mb-1'>{props.meal.name}</h3>
         <div className='italic ml-6 mb-1'>{props.meal.description}</div>
-        <div className='font-bold text-md ml-6 text-amber-950 mb-2'>{price}</div>
+        <div className='font-bold text-md ml-6 text-amber-900 mb-2'>{price}</div>
+    </div>
+    <MealItemForm />
     </div>
     <hr className='mx-auto w-[45rem] ' />
   </li>
--- client/src/components/Meals/MealItem/MealItemForm.jsx
+++ client/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function MealItemForm() {
   return (
+    <Fragment>
     <div>
       
+    <div className='flex items-end justify-end mb-2 mt-2'>
+      <label htmlFor="amount" className='font-bold'>Amount</label>
+      <input type="number" name="amount" id="amount" className='h-[30px] w-[3rem] rounded-md bg-white ml-2' />
     </div>
+
+    <div>
+     <button type="button" className='rounded-xl text-white bg-amber-900 ml-4 w-[5rem]'>+ Add</button>
+    </div>
+    </div>
+    </Fragment>
+
   )
 }
 
